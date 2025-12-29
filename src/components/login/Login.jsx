5a439427d270fe814c6db67f9a6e921cb12b5ff6
jihanurrahman33 <jihanurrahman33@gmail.com>
2025-12-29 23:50:15 +0600

    feat: add SocialButton component for Google authentication; integrate with Register and Login pages

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -4,10 +4,13 @@ import React from "react";
 import { FaEnvelope, FaLock } from "react-icons/fa";
 import { signIn } from "next-auth/react";
 import Swal from "sweetalert2";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import SocialButton from "../buttons/SocialButton";
 
 const Login = () => {
+  const params = useSearchParams();
   const router = useRouter();
+  const callBack = params.get("callbackUrl") || "/";
   const handleSubmit = async (e) => {
     e.preventDefault();
     const form = e.target;
@@ -19,7 +22,8 @@ const Login = () => {
     const result = await signIn("credentials", {
       email: userData.email,
       password: userData.password,
-      redirect: false,
+      //redirect: false,
+      callbackUrl: params.get("callbackUrl") || "/",
     });
     if (!result.ok) {
       Swal.fire({
@@ -34,7 +38,6 @@ const Login = () => {
         text: "You have been logged in successfully.",
       });
       form.reset();
-      router.push("/");
     }
   };
   return (
@@ -89,44 +92,17 @@ const Login = () => {
             </button>
           </form>
 
-          <button className="btn bg-white text-black border-[#e5e5e5] w-full">
-            <svg
-              aria-label="Google logo"
-              width="16"
-              height="16"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-            >
-              <g>
-                <path d="m0 0H512V512H0" fill="#fff"></path>
-                <path
-                  fill="#34a853"
-                  d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
-                ></path>
-                <path
-                  fill="#4285f4"
-                  d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
-                ></path>
-                <path
-                  fill="#fbbc02"
-                  d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
-                ></path>
-                <path
-                  fill="#ea4335"
-                  d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
-                ></path>
-              </g>
-            </svg>
-            Login with Google
-          </button>
-
+          <SocialButton></SocialButton>
           {/* Divider */}
           <div className="divider">OR</div>
 
           {/* Register */}
           <p className="text-center text-sm">
             Don’t have an account?{" "}
-            <Link href="/register" className="link link-primary">
+            <Link
+              href={`register?callbackUrl=${callBack.slice(1)}`}
+              className="link link-primary"
+            >
               Register
             </Link>
           </p>
